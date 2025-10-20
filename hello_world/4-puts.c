9477bfb1037f6cf890a207c96df048c763b9c2a9
umma29360 <umma29@hotmail.fr>
2025-10-20 12:32:53 +0000

    ajout fichier 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entréé
+ *
+ * Return: toujours  0 
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
-return (0);
+    return (0);
 }

babfe86f30160dc2a4ce6f70c172e28a144005a8
root <umma29@hotmail.fr>
2025-10-21 11:54:08 +0000

    ajout fichier 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**                                                                                           
+ * main - Entry point of the program                                                          
+ *                                                                                            
+ * Description: Prints the alphabet in lowercase letters                                      
+ *              followed by a new line.                                                       
+ *                                                                                            
+ * Return: Always 0 (Success)                                                                 
+ */
+int main(void)
+{
+  char i = 'a';
+
+  while (i <= 'z')
+    {
+      putchar(i);
+      i++;
+    }
+  char m = 'A';
+
+  while (m <= 'Z')
+    {
+      putchar(m);
+      m++;
+    }
+    putchar('\n');
+    return (0);
+}
+
+
+

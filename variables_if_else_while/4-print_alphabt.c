6a75891b03765da76f53d3f1d61ed01fdc4e7815
root <umma29@hotmail.fr>
2025-10-21 12:08:59 +0000

    ajout fichier 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**                                                                                           
+ * main - Entry point of the program                                                          
+ *                                                                                            
+ * Description: Prints the alphabet in lowercase letters                                      
+ *              followed by a new line.                                                       
+ *                                                                                            
+ * Return: Always 0 (Success)                                                                 
+ */
+int main(void)
+{
+ int char i;
+
+ i = a; 
+  while (i <= 'z' && i != 'q''e')
+    {
+      putchar(i);
+      i++;
+    }
+    putchar('\n');
+    return (0);
+}
+
+
+
+
+
